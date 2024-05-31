0b99b4258b0f574ce6cd2e7d6765364eae08f4b4
Harsh Kumar <harsh20221@gmail.com>
2024-06-01 04:20:20 +0530

    Javascript tutorials

--- Fundamentals-1/Starter/script.js
+++ Fundamentals-1/Starter/script.js
@@ -2,3 +2,15 @@ console.log("Hello World"); /////THIS IS MY FIRST HELLO WORLD IN JS//////
 console.log(22+22+33+88);
 let name='Harsh'
 console.log("Helloworld")
+
+let x=1900;
+console.log(x);  //? This is the assignment operator 
+
+
+let sarah=2020-x
+if(sarah<=99){
+    console.log("FIT")
+}
+else {
+    console.log("EXCEED")
+}
