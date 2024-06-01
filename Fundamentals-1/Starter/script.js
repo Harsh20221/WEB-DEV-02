54d6ba08bbefa223722356919d453a034c7ef346
Harsh Kumar <harsh20221@gmail.com>
2024-06-01 08:26:58 +0530

    Experimenting with JS code

--- Fundamentals-1/Starter/script.js
+++ Fundamentals-1/Starter/script.js
@@ -1,4 +1,4 @@
-console.log("Hello World"); /////THIS IS MY FIRST HELLO WORLD IN JS////// 
+/* console.log("Hello World"); /////THIS IS MY FIRST HELLO WORLD IN JS////// 
 console.log(22+22+33+88);
 let name='Harsh'
 console.log("Helloworld")
@@ -15,6 +15,46 @@ else {
     console.log("EXCEED")
 }
 
+ */
+/* var miramar= "2077";
+/* console.log(Number(miramar)); */ //? This'll change the type of this variable to /* Number, Make sure to use capital N with number  
+/* console.log("22"/"11")  */ //? This is the example of type coersion in Jvascript where Js changes the type automatically  
 
-var miramar= "2077";
-console.log(Number(miramar)); //? This'll change the type of this variable to number 
+
+
+/* 
+const favourite = Number(prompt("Please Enter Favourite No!!!"))  //# You can use prompt to get a input from the user in web Browser 
+
+ if ( favourite<=50){
+    alert("NNumber Lies in 1 Quadrant") //# You can use alert to give output to user in web Browser
+ }
+ else alert("Number lies in 2 Quadrant") */
+
+
+
+
+/* 
+ taming=String(prompt("Please Enter your Taming Food "))
+ dart=String(prompt("please enter Dart type"))
+if(taming==="kibble" && dart==="shocking"){
+    alert("Taming will take least amount  ")
+}
+
+else 
+alert("Taming will take variable amount") */
+name=String(prompt("Please enter name"))
+
+switch(name){
+    case("Peterparker"):  //?DO not forget to use : after writing case statement 
+    alert("We have spiderman");
+    break;
+
+    case ("Tonystark"):
+        alert("We have Ironman");
+        break; //? Do not forget to use break statement 
+
+    case("Wanda"):
+    alert("You have the Scarlet Witch ");
+    break;
+
+}
