79b5f25cd852812e4dc77a5ff69fefe1587b78f6
Harsh Kumar <harsh20221@gmail.com>
2024-06-03 17:43:30 +0530

    Linked DOM

--- Dom Manipulation-1/script.js
+++ Dom Manipulation-1/script.js
@@ -1,2 +1,11 @@
-    'use strict';
-console.log(document.querySelector('.message').textContent);    
+    'use strict';  //* Using Dom Manipulation to edit / change various HTML elements demonstration 
+/* console.log(document.querySelector('.message').textContent);    
+
+document.querySelector('.message').textContent='🎉Correct Number';
+
+document.querySelector('.score').textContent=22; */
+///THE ACTUAL CODE FOR THIS PROJECT STARTS UP FROM HERE /////
+
+document.querySelector('.check').addEventListener('click', function (){  //? This Method will display the value of input field after pressing the check button, This function will help to display the value of the text field to the console 
+console.log(document.querySelector('.guess').value)
+});
