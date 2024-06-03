e3fb25a6b868481f6cea4e469da9489652e11f92
Harsh Kumar <harsh20221@gmail.com>
2024-06-03 19:40:38 +0530

    added mod

--- Dom Manipulation-1/script.js
+++ Dom Manipulation-1/script.js
@@ -5,11 +5,13 @@ document.querySelector('.message').textContent='🎉Correct Number';
 
 document.querySelector('.score').textContent=22; */
 ///THE ACTUAL CODE FOR THIS PROJECT STARTS UP FROM HERE /////
-const number = Math.trunc(Math.random()*20)+1;
+const secretNumber = Math.trunc(Math.random()*20)+1;
 ///?Math. random generates a random floating poingt no btw 0 and 1  we are multiplying that no by 20 to get random no btw 0 and 20 and then using Math.trunc to omit the fractional point to get whole numbers  
+const guess=Number(document.querySelector('.guess').value);
+document.querySelector('.number').textContent=secretNumber;
 
 document.querySelector('.check').addEventListener('click', function (){  //? This Method will display the value of input field after pressing the check button, This function will help to display the value of the text field to the console 
-const guess=Number(document.querySelector('.guess').value);
+
 if (guess==null){
     document.querySelector('.message').textContent="⛔ No Number!!!!!"
 }
