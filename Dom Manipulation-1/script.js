f67629441f3a59ebcac3a432542da1ff37de48a5
Harsh Kumar <harsh20221@gmail.com>
2024-06-04 03:09:46 +0530

    Fixed score and other properties

--- Dom Manipulation-1/script.js
+++ Dom Manipulation-1/script.js
@@ -9,10 +9,11 @@ const secretNumber = Math.trunc(Math.random()*20)+1;
 ///?Math. random generates a random floating poingt no btw 0 and 1  we are multiplying that no by 20 to get random no btw 0 and 20 and then using Math.trunc to omit the fractional point to get whole numbers  
 
 document.querySelector('.number').textContent=secretNumber; //?. The secret no is shown to user currently only for testing purposes 
+let score = 20;  //!!! Do not define this score variable inside the event listener block else it'll reset again and again to 20 and the program will not work 
 
 document.querySelector('.check').addEventListener('click', function (){  //? This Method will display the value of input field after pressing the check button, This function will help to display the value of the text field to the console 
     const guess=Number(document.querySelector('.guess').value);   //! Do not put this Statement outside of the loop , This Statement is responsible for storing our guess ( Input Number S)
-let score = 20;
+
 
 if (guess==null){
     document.querySelector('.message').textContent="⛔ No Number!!!!!"
@@ -23,12 +24,27 @@ else if (guess === secretNumber){
 }
 
 else if(guess>secretNumber) {
-    document.querySelector('.message').textContent='📈 Too High';
+ if(score>0){
+ document.querySelector('.message').textContent='📈 Too High';
+score--;
+document.querySelector('.score').textContent=score; //? Here we are assigning thenscore that is displayed in the screen created in the html file to the score variable we have created here 
+
+}
+    else {
+        document.querySelector('.message').textContent='💥 You have lost the game TRY again '
+    }
 
 }
 else if ( guess < secretNumber){
+    if (score>0){
     document.querySelector('.message').textContent='📉 Too Low '
     score -- ;
-    document.querySelector('score').textContent=score;
+    document.querySelector('.score').textContent=score; ///! Very imp - if you want to access any html class then do not forget to write like this ---- '.score' , it means do not forget to use dot before listing the required parameter 
+}
+
+else {
+    document.querySelector('.message').textContent='💥 You have lost the game TRY again '
+}
+
 }
 });
