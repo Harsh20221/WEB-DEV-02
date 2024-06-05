74a2f67d8cb4ce775dc0eccf4e25d7f047f06913
Harsh Kumar <harsh20221@gmail.com>
2024-06-05 07:44:05 +0530

    Hoisting

--- Behind the scenes/08-Behind-the-Scenes/starter/script.js
+++ Behind the scenes/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+//* Hoisting and TheDeadZone
+vary1=addnum(2,3)
+console.log(vary1)  //? This will give an error as we know that functiin does not support hoisting and we have defined addnum later in the code after using it hence it'll give errors 
+
+function addnum(a,b){
+    return a+b;
+
+
+}
+
+
+
+
+
