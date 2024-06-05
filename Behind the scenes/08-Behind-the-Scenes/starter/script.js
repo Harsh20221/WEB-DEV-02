a16c96dcccee6f15f34570b7879c92e50f5c4a29
Harsh Kumar <harsh20221@gmail.com>
2024-06-05 07:45:53 +0530

    Hoisting

--- Behind the scenes/08-Behind-the-Scenes/starter/script.js
+++ Behind the scenes/08-Behind-the-Scenes/starter/script.js
@@ -3,7 +3,7 @@
 vary1=addnum(2,3)
 console.log(vary1)  //? This will give an error as we know that functiin does not support hoisting and we have defined addnum later in the code after using it hence it'll give errors 
 
-function addnum(a,b){
+function addnum(a,b){ //? Till this function is not used it is in the dead zone , We have used this function before it is declared hence it'll give error as currently it is in dead zone , JS knows it exist but is undefined until initialised 
     return a+b;
 
 
