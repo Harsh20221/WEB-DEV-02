b3495bcf5c69e8b607a88d8b32032925991919d5
Harsh <harsh20221@gmail.com>
2024-07-02 03:26:02 +0530

    Comments nd functional Changes

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -29,8 +29,38 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
-
-
+const header = document.querySelector('.header');
+//* HERE we are using different Dom elements selection methods
 document.getElementById('section--1'); //?? This is a Dom selection method to get element by ID
 const allButtons=document.getElementsByTagName('button')
 //? This is a Dom selection Method to get element by TagName
+/*console.log(allButtons)*/; //? This'll give a HTML Collection of all Buttons
+
+//# This method below helps to create the cookie popup
+const message = document.createElement('div')
+message.classList.add('cookie-message');
+message.innerHTML = 'We Use Cookies for improved functionality and Analytics.  , <button class  ="btn  btn--close-cookie">Got it ! </button> ';
+header.prepend(message);
+header.append(message);
+
+///# This method below is responsible for clearing Cookie Message
+document.querySelector('.btn--close-cookie').addEventListener('click',function(){
+  message.remove()
+})
+
+////////////Below Here we Start the Styles Section /////////
+
+
+
+
+message.style.backgroundColor='#37383d'
+message.style.width='120%';
+/*console.log(message.style.color); *////? This'll give us the message color
+/*console.log(message.style.backgroundColor)*/ //? This'll give us the message bacground color
+/*console.log(getComputedStyle(message).color)
+console.log(getComputedStyle(message).height)*/
+
+
+message.style.height= Number.parseFloat(getComputedStyle(message).height,10)+30+'px';  //? This'll give us the message height , the 10 is the base of the number by base we mean the radix of the number
+//?  .parseFloat() is used to convert a string to a number then we add 30 to it and add 'px' to it to make it a string because the height property is a string
+console.log(message.style.height);
